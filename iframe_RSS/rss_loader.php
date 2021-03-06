f337bca87cc48d254da170900f38c748d36b4127
bluesnooker123 <dev333perfect@gmail.com>
2021-03-06 13:37:10 +0700

    Final Commit

---
+++ iframe_RSS/rss_loader.php
@@ -0,0 +1,331 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Current Openings</title>
+		<meta name="description" content="">
+		<meta name="author" content="PeopleHR">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="apple-mobile-web-app-capable" content="yes"/>
+		<meta http-equiv="X-UA-Compatible" content="chrome=1">
+		
+		<!-- CSS
+	 	 ================================================== -->
+		<style>
+        
+html, body,div,dl,dt,dd,ul,ol,li,
+h1,h2,h3,h4,h5,h6,
+form,fieldset,legend,input,button,textarea,p,blockquote,th,td,
+span, object, iframe, pre, abbr, address, article, section, header,
+hgroup, menu, nav, time, mark, audio, video, details, canvas, summary { 
+	background: transparent;
+	margin: 0;
+	padding: 0;
+	outline: 0;
+	font-size: 100%;
+	}
+
+article,aside,details,figcaption,figure,
+footer,header,hgroup,menu,nav,section {
+	display: block;
+}	
+	
+table { 
+	border-collapse: collapse;
+	border-spacing: 0;
+	}
+	
+fieldset,img,button { 
+	border: 0 none;
+	}
+	
+address,caption,cite,code,dfn,em,th,var,optgroup { 
+	font-style:inherit;
+	font-weight:inherit;
+	}
+
+del,ins { 
+	text-decoration:none;
+	}
+
+ol { 
+	list-style:decimal;
+	}
+	ol li { 
+		list-style:decimal outside;
+		}
+ul { 
+	list-style:none;
+	}
+	
+blockquote, q {
+    quotes:none;
+}
+
+caption,th { 
+	text-align:left;
+	}
+
+h1,h2,h3,h4,h5,h6 { 
+	font-size:100%;
+	font-weight:normal;
+	}
+
+:focus { outline:0;}
+
+blockquote:before, blockquote:after,
+q:before, q:after {
+    content:'';
+    content:none;
+}
+
+abbr,acronym{ border:0 none;font-variant:normal;}
+sup{ vertical-align:text-top;}
+sub{ vertical-align:text-bottom;}
+legend{ color:#000;}
+
+input,button,textarea,select,optgroup,option { 
+	font-family:inherit;
+	font-size:inherit;
+	font-style:inherit;
+	font-weight:inherit;
+	vertical-align:middle;
+	}
+	
+input,textarea,select { 
+	background: #fff;
+	border:1px solid #CCC;
+	}
+        @import "http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700|Signika:400,600";
+
+*{
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	-ms-box-sizing: border-box;
+	box-sizing: border-box; 
+}
+a, a:visited { text-decoration: none; }
+a:hover { transition: color 0.3s linear 0s, background 0.2s linear 0s; }
+.clearfix::after {
+	clear: both;
+	content: " ";
+	display: block;
+	height: 0;
+	visibility: hidden;
+}
+html:after {
+	clear: both;
+	content: " ";
+	display: block;
+	height: 0;
+	visibility: hidden;
+}
+
+body {
+	background: #ffffff none repeat scroll 0 0;
+	color: #333333;
+	font-family: "Open Sans",sans-serif;
+	font-size: 14px;
+	overflow-x: hidden;
+}
+
+.container{
+	margin: 0px auto;
+	padding: 0px 10px;
+	max-width: 960px;
+	width: 100%;
+}
+
+header { background-color: #111;
+    height: 70px;
+    padding: 10px 0;}
+	header h1.logo {
+		display: block;
+		float: left;
+		margin-bottom: 0;
+		 line-height: 50px;
+ 
+    padding-left: 10px;
+	}
+		header h1.logo a{ display: block; }
+			h1.logo a img { 
+				max-height: 75px; 
+				vertical-align: middle;
+				width: auto; 
+			}
+		
+section.bgwhite {
+	background-color: #ffffff;
+	border: 1px solid #cdcdcd;
+	border-radius: 3px;
+	margin-top: 20px;
+	overflow: hidden;
+	padding: 0px;
+	margin-bottom: 20px;
+	text-align: left;
+}	
+
+ul.share-buttons {
+	float: right;
+	margin-bottom: 0;
+	margin-top: 45px;
+	padding: 0;
+}
+	ul.share-buttons li {
+		display: block;
+		float: left;
+		margin-right: 10px;
+	}
+		ul.share-buttons li:last-child { margin-right: 0; }
+		ul.share-buttons li a {
+	    background-color: #f1f1f1;
+	    border-radius: 100%;
+	    color: #999999;
+	    display: block;
+	    font-size: 15px;
+	    height: 30px;
+	    line-height: 28px;
+	    text-align: center;
+	    width: 30px;
+		}
+
+.privacy{
+	color: #999999;
+	display: block;
+	float: left;
+	font-weight: 600;
+	font-size: 11px;	
+}	
+	.privacy i{
+		display: inline-block; 
+		font-size: 13px;
+		margin-right: 5px;
+	}
+
+table {
+	color: #666666;
+	cursor: pointer;
+	font-size: 13px;
+	width: 100%;
+}
+	table tbody:hover td {
+		-webkit-transition: background linear 0.2s;
+		-moz-transition: background linear 0.2s;
+		-o-transition: background linear 0.2s;
+		-ms-transition: background linear 0.2s;
+		transition: background linear 0.2s;
+	}
+	
+	table.style1 .title {
+    color: #446b8e;
+    display: block;
+    font-size: 18px;
+    font-weight: 600;
+    padding-top: 7px;
+    text-shadow: 1px 1px #fff;
+	}
+	table.style1 tbody tr:hover td{ background-color: #EDFAFF !important; }
+	table.style1 thead { 
+		border-top: 1px solid #DCDCDC;
+		border-right: 1px solid #DCDCDC; 
+		border-left: 1px solid #DCDCDC; 
+		border-bottom: 1px solid #DCDCDC; 
+		box-shadow: 0 1px #FFFFFF inset; 
+		-webkit-box-shadow: 0 1px #FFFFFF inset;
+	}
+		table.style1 thead tr { background-color: #EDEDED; }
+		table.style1 thead tr th{
+	    border-right: 1px solid #dcdcdc;
+	    font-size: 14px;
+	    padding: 0 15px 10px;
+		}
+		table.style1 tbody { border: 1px solid #DCDCDC; }
+		table.style1 tbody tr:nth-child(2n) { background-color: #F8F8F8; }
+		table.style1 tbody tr th,
+		table.style1 tbody tr td {
+			border-right: 1px solid #DCDCDC;
+			line-height: 20px;
+			padding: 8px 10px;
+		}
+		table.style1 tbody tr td i {
+	    color: #1E99D6;
+	    display: inline-block;
+	    font-size: 15px;
+	    margin-right: 5px;
+		}
+		table.style1 tbody tr td span.small{ display: block; color: #999999;padding-left:20px; }
+		table.style1 tbody tr td:first-child{
+			color: #446b8e;
+			font-size: 14px;
+			font-weight: 600;
+		}
+
+
+
+
+
+
+/* Responsive
+================================================== */	
+@media (max-width: 478px) {
+	section.bgwhite{ padding: 10px; }
+	table.style1 tbody tr td:first-child{ font-size: 12px; }
+}
+
+        </style>
+		
+		<!--[if lt IE 9]>
+		<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+		<![endif]-->
+	
+	</head>
+	
+  <body>
+  		
+  	
+  	<div class="container">
+			<section class="bgwhite">
+		
+				<table class="style1">
+					<thead>
+						<tr>
+							<th colspan="4"><span class="title">Current Openings</span></th>
+						</tr>
+						<tr>
+							<th colspan="4">Thanks for checking out our job openings. See something that interests you? Apply here.</th>
+						</tr>
+					</thead>
+					<tbody>
+                    
+                    <?php
+// Replace the value of $url with the  vacancies URL taken from the People app (Settings->ATS->RSS Feed)
+$url="https://warwickestates.peoplehr.net/Pages/JobBoard/CurrentOpenings.aspx?o=1bc8ccdf-77b7-44e6-88db-d7654e36e4d1";
+
+//the XML file load
+$file=simplexml_load_file($url);
+$vacancies=$file->channel->item;
+
+//Cicle trough each vacancy and display it
+for($a=0;$a<count($vacancies);$a++)
+	{
+		echo'	<tr>
+							<td ><a href="'.$vacancies[$a]->link.'" style="color:#446b8e" target="_blank">'.$vacancies[$a]->title.'</a></td>
+							<td>Description<span class="small">'.$vacancies[$a]->vacancydescription.'</span></td>
+							<td>Location<span class="small">'.$vacancies[$a]->location.'</span></td>
+							<td>Department<span class="small">'.$vacancies[$a]->department.'</span></td>
+						</tr>';
+	}?>
+                    
+					
+					
+					</tbody>
+				</table>
+			</section>
+			
+		</div>
+ 
+		  
+  
+    
+  </body>
+</html>
